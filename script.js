0b78aa615fd8be565db3cb23bc0730aeb8ea18e8
JurkinS <example@gmail.com>
2019-06-22 16:53:28 +0300

    Add script file

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let incr = 10,
+    decr = 10;
+
+// Префиксная форма - возвращает изменёное значение
+/*incr++; // 10+1
+decr--; // 10-1
+console.log(incr);
+console.log(decr);
+console.log(++incr);
+console.log(--decr);
+*/
+
+//Постфиксная форма - возвращает первоначальное значение
+/*console.log(incr++);
+console.log(decr--);
+*/
+
+//Остаток от деления
+//console.log(5 % 2);
+
+//Оператор "="
+//console.log(2 = 2);      // оператор присваивания
+//console.log("2" == 2);     // оператор сравнения
+//console.log("2" === 2);    // оператор сравнения по типу данных
+
+//Операторы "|| or" "&& and"
+/*let isChecked = true,
+    isClosed = true;
+
+console.log(isChecked && isClosed);
+*/
